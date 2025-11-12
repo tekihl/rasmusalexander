9345299f870574b2d0354c160c5be2a032d3ab1e
“Tom <tom.kihlborg@gmail.com>
2025-11-12 17:29:16 +0100

    fixed homepage construction

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import './projects.scss'
 import Typed from 'typed.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const typed = new Typed('#element', {
-    strings: ['website under construction'],
-    typeSpeed: 50,
-  });
+    new Typed('#element', {
+        strings: ['website under construction'],
+        typeSpeed: 50,
+    });
 });
