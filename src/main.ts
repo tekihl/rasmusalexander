9fd3499a29afa80a8b91d73fe2cb43f71bf317f7
“Tom <tom.kihlborg@gmail.com>
2025-11-13 11:46:35 +0100

    new height on mobile page

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import Typed from 'typed.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     new Typed('#element', {
-        strings: ['website under construction'],
+        strings: ['web^50site ^150 under ^200 co^20nstru^50ction'],
         typeSpeed: 50,
+        startDelay: 1500,
     });
 });
